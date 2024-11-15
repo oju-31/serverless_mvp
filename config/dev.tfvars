1e564f14846689ecb171fef16845e7e78a16ed3c
oju-31 <ojutomoria@gmail.com>
2024-11-15 20:06:46 +0000

    terraform set up

---
+++ config/dev.tfvars
@@ -0,0 +1,3 @@
+ENV                      = "dev"
+ROUTE53_HOSTED_ZONE_NAME = "demo.powersmarter.net"
+WEBAPP_DNS               = "demo.powersmarter.net" 
--- terraform.tf
+++ terraform.tf
@@ -5,6 +5,17 @@ terraform {
     dynamodb_table = "smarterise-webapp-aws-terraform-locks-centralized"
     region         = "us-east-1"
     key            = "smarterise-webapp-backend-infra/{{ENV}}/terraform.tfstate"
-    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
   }
 }
+
+provider "aws" {}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
