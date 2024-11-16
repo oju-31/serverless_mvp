9c62b94edaae6f895541515e5094c3d9f725c167
oju-31 <ojutomoria@gmail.com>
2024-11-16 12:43:02 +0000

    updates

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   DOMAIN_NAME         = "api.${var.WEBAPP_DNS}"
   RESOURCE_PREFIX     = "${var.ENV}-smarterise"
   ACCOUNTID           = data.aws_caller_identity.current.account_id
-  cognito_domain_name = lower(var.ENV) == "prod" ? "auth.${var.WEBAPP_DNS}" : "auth.${var.WEBAPP_DNS}"
+  cognito_domain_name = "auth.${var.WEBAPP_DNS}"
   common_tags         = { 
     environment = var.ENV 
     project     = "smarterise-webapp" 
