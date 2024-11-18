d992c740ef9d2bc7af38a147073abdea8291c54c
oju-31 <ojutomoria@gmail.com>
2024-11-18 14:45:40 +0000

    api plan

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-tf-example-bucket-999234"
 }
 
-######################################################################
+###########################################
 # LAMBDA MODULES
-######################################################################
+###########################################
 module "lambdas" {
   source             = "./infra/lambdas"
   ENV                = var.ENV
@@ -36,10 +36,78 @@ module "lambdas" {
 }
 
 
-################################################################################
+############################################
 # IAM ROLE MODULES
-################################################################################
+############################################
 module "roles" {
   source          = "./infra/iam_roles"
   RESOURCE_PREFIX = local.RESOURCE_PREFIX
+}
+
+
+###########################################
+# LAMBDA MODULES
+###########################################
+module "open" {
+  source          = "./infra/api_gateways/open_endpoints"
+  ENV             = var.ENV
+  COMMON_TAGS     = local.common_tags
+  CURRENT_ACCOUNT_ID       = local.ACCOUNTID
+  API_DOMAIN_NAME = module.custom_domain.API_DOMAIN_NAME
+  LAMBDA_NAMES = [
+    module.lambda.LAMBDA_SIGNUP_NAME,
+    module.lambda.LAMBDA_CONTACT_US_NAME,
+    module.lambda.LAMBDA_LOGIN_NAME,
+    module.lambda.LAMBDA_CONFIRM_SIGNUP_NAME,
+    module.lambda.LAMBDA_RESEND_VERIFY_CODE_NAME,
+    module.lambda.LAMBDA_CHANGE_PASSWORD_NAME,
+    module.lambda.LAMBDA_FORGOT_PASSWORD_NAME,
+    module.lambda.LAMBDA_CONFIRM_FORGOT_PASSWORD_NAME,
+    module.lambda.LAMBDA_REFRESH_TOKEN_NAME,
+    module.lambda.LAMBDA_RANKING_NAME
+
+    ]
+  LAMBDA_SIGNUP_ARN  = module.lambda.LAMBDA_SIGNUP_ARN
+  LAMBDA_SIGNUP_NAME = module.lambda.LAMBDA_SIGNUP_NAME
+  LAMBDA_SIGNUP_ROLE_ARN    = module.role.LAMBDA_SIGNUP_ROLE_ARN
+  
+  LAMBDA_REFRESH_TOKEN_ARN  = module.lambda.LAMBDA_REFRESH_TOKEN_ARN
+  LAMBDA_REFRESH_TOKEN_NAME = module.lambda.LAMBDA_REFRESH_TOKEN_NAME
+  LAMBDA_REFRESH_TOKEN_ROLE_ARN    = module.role.LAMBDA_REFRESH_TOKEN_ROLE_ARN
+  
+  LAMBDA_CONFIRM_SIGNUP_ARN  = module.lambda.LAMBDA_CONFIRM_SIGNUP_ARN
+  LAMBDA_CONFIRM_SIGNUP_NAME = module.lambda.LAMBDA_CONFIRM_SIGNUP_NAME
+  LAMBDA_CONFIRM_SIGNUP_ROLE_ARN    = module.role.LAMBDA_CONFIRM_SIGNUP_ROLE_ARN
+  
+  LAMBDA_LOGIN_ARN  = module.lambda.LAMBDA_LOGIN_ARN
+  LAMBDA_LOGIN_NAME = module.lambda.LAMBDA_LOGIN_NAME
+  LAMBDA_LOGIN_ROLE_ARN    = module.role.LAMBDA_LOGIN_ROLE_ARN
+  
+  LAMBDA_RESEND_VERIFY_CODE_ARN  = module.lambda.LAMBDA_RESEND_VERIFY_CODE_ARN
+  LAMBDA_RESEND_VERIFY_CODE_NAME = module.lambda.LAMBDA_RESEND_VERIFY_CODE_NAME
+  LAMBDA_RESEND_VERIFY_CODE_ROLE_ARN    = module.role.LAMBDA_RESEND_VERIFY_CODE_ROLE_ARN
+  
+  LAMBDA_CONTACT_US_ARN  = module.lambda.LAMBDA_CONTACT_US_ARN
+  LAMBDA_CONTACT_US_NAME = module.lambda.LAMBDA_CONTACT_US_NAME
+  LAMBDA_CONTACT_US_ROLE_ARN    = module.role.LAMBDA_CONTACT_US_ROLE_ARN
+  
+  LAMBDA_CHANGE_PASSWORD_ARN  = module.lambda.LAMBDA_CHANGE_PASSWORD_ARN
+  LAMBDA_CHANGE_PASSWORD_NAME = module.lambda.LAMBDA_CHANGE_PASSWORD_NAME
+  LAMBDA_CHANGE_PASSWORD_ROLE_ARN    = module.role.LAMBDA_CHANGE_PASSWORD_ROLE_ARN
+  
+  LAMBDA_FORGOT_PASSWORD_ARN  = module.lambda.LAMBDA_FORGOT_PASSWORD_ARN
+  LAMBDA_FORGOT_PASSWORD_NAME = module.lambda.LAMBDA_FORGOT_PASSWORD_NAME
+  LAMBDA_FORGOT_PASSWORD_ROLE_ARN    = module.role.LAMBDA_FORGOT_PASSWORD_ROLE_ARN
+  
+  LAMBDA_CONFIRM_FORGOT_PASSWORD_ARN  = module.lambda.LAMBDA_CONFIRM_FORGOT_PASSWORD_ARN
+  
+  LAMBDA_RANKING_ARN  = module.lambda.LAMBDA_RANKING_ARN
+  LAMBDA_RANKING_NAME = module.lambda.LAMBDA_RANKING_NAME
+  LAMBDA_RANKING_ROLE_ARN    = module.role.LAMBDA_RANKING_ROLE_ARN
+  
+  depends_on = [
+      module.roles,
+      module.lambdas
+    ]
+
 }
