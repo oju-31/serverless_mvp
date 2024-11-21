95c2dac4758710444b5b5f67704065ac5080ac0c
oju-31 <ojutomoria@gmail.com>
2024-11-21 19:04:40 +0000

    change of account

--- infra/api_gateways/open_endpoints/vars.tf
+++ infra/api_gateways/open_endpoints/vars.tf
@@ -5,3 +5,7 @@ variable "API_DOMAIN_NAME" {}
 variable "RESOURCE_PREFIX" {
   default = "open"
 }
+variable "LAMBDA_NAMES" {
+  description = "contains Names of lambda(s) to be added into <aws_lambda_permission> resource"
+  type        = list(string)
+}
