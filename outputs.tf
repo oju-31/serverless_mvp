9c966cf7b8d11148b6a29f6b9e5cce1ab80d9ff7
MAC <mac@Ojus-Macbook-Air.local>
2025-04-11 17:26:04 +0200

    terraform rearranged3

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,8 @@ matches the one on your tvars. The poolid is the one right after https://.
 Also, ??the CLOUDFRONT_DOMAIN_NAME output returns a url without https
 preceding, so that has been done with the slashes escaped
 EOT
-  value = "https://us-east-1uxrlifhg5.auth.us-east-1.amazoncognito.com/login?client_id=3t0ddf7eo26rhtm56q982v9a0p&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2F${module.static.CLOUDFRONT_DOMAIN_NAME}"
+  # value = "https://us-east-1uxrlifhg5.auth.us-east-1.amazoncognito.com/login?client_id=3t0ddf7eo26rhtm56q982v9a0p&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2F${module.static.CLOUDFRONT_DOMAIN_NAME}"
+  value = "this is a placeholder"
 }
 
 #-------------------------------
