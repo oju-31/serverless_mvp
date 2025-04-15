d2c760b350a04972ff7d87ef5305d2d79bebdc61
MAC <mac@Ojus-Macbook-Air.local>
2025-04-15 18:25:34 +0200

    backen url testing4

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,6 @@
 // config.js
 export const config = {
     lambdaUrl: 'https://i4otrcsddqhqiuhddlgq6e7lrm0arlol.lambda-url.us-east-1.on.aws/',
-    appVersion: '1.0.0'
+    appVersion: '1.0.0',
+    type: "module",
   };
