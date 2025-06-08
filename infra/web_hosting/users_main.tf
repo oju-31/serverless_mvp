1bb7efffd3c3ca4d7ea156aee6437686d0024478
Adijat Ojutomori <mac@Ojus-Macbook-Air.local>
2025-06-08 22:02:36 +0200

    fixes: bucket policy to add

--- infra/web_hosting/users_main.tf
+++ infra/web_hosting/users_main.tf
@@ -91,35 +91,35 @@ resource "aws_s3_bucket_public_access_block" "allow_public_access" {
 }
 
 # this is necesarry if you are deploying to an aws account that is not yours
-# resource "aws_s3_bucket_policy" "allow_public_access_and_gha" {
-#   bucket = aws_s3_bucket.ahlorq_frontend.id
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Sid       = "PublicReadGetObject"
-#         Effect    = "Allow"
-#         Principal = "*"
-#         Action    = "s3:GetObject"
-#         Resource  = "${aws_s3_bucket.ahlorq_frontend.arn}/*"
-#       },
-#       {
-#         Sid      = "AllowGitHubActionsSync"
-#         Effect   = "Allow"
-#         Principal = {AWS = "${var.GITHUB_ACTIONS_USER_ARN}"}
-#         Action   = [
-#           "s3:ListBucket",
-#           "s3:PutObject",
-#           "s3:DeleteObject"
-#         ]
-#         Resource = [
-#           "${aws_s3_bucket.ahlorq_frontend.arn}",
-#           "${aws_s3_bucket.ahlorq_frontend.arn}/*"
-#         ]
-#       }
-#     ]
-#   })
-# }
+resource "aws_s3_bucket_policy" "allow_public_access_and_gha" {
+  bucket = aws_s3_bucket.ahlorq_frontend.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.ahlorq_frontend.arn}/*"
+      },
+      {
+        Sid      = "AllowGitHubActionsSync"
+        Effect   = "Allow"
+        Principal = {AWS = "${var.GITHUB_ACTIONS_USER_ARN}"}
+        Action   = [
+          "s3:ListBucket",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.ahlorq_frontend.arn}",
+          "${aws_s3_bucket.ahlorq_frontend.arn}/*"
+        ]
+      }
+    ]
+  })
+}
 
 # ---------------------------------------------
 # CLOUD FRONT DISTRIBUTION 
