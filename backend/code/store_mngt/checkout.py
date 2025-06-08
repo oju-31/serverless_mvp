2cf672d1568732775a37e629d8f66d7a9ee46af7
Adijat Ojutomori <mac@Ojus-Macbook-Air.local>
2025-06-08 21:10:01 +0200

    restructuring to 4 lambdas 4

---
+++ backend/code/store_mngt/checkout.py
@@ -0,0 +1,3 @@
+import boto3
+import json
+from botocore.exceptions import ClientError
--- infra/lambdas/func_data.tf
+++ infra/lambdas/func_data.tf
@@ -1,23 +1,23 @@
 data "archive_file" "sewing_guides" {
   type        = "zip"
-  source_dir  = "${path.root}/src/code/sewing_guides"
-  output_path = "${path.root}/src/zip/sewing_guides.zip"
+  source_dir  = "${path.root}/backend/code/sewing_guides"
+  output_path = "${path.root}/backend/zip/sewing_guides.zip"
 }
 
 data "archive_file" "store_mngt" {
   type        = "zip"
-  source_dir  = "${path.root}/src/code/store_mngt"
-  output_path = "${path.root}/src/zip/store_mngt.zip"
+  source_dir  = "${path.root}/backend/code/store_mngt"
+  output_path = "${path.root}/backend/zip/store_mngt.zip"
 }
 
 data "archive_file" "style_generations" {
   type        = "zip"
-  source_dir  = "${path.root}/src/code/style_generations"
-  output_path = "${path.root}/src/zip/style_generations.zip"
+  source_dir  = "${path.root}/backend/code/style_generations"
+  output_path = "${path.root}/backend/zip/style_generations.zip"
 }
 
 data "archive_file" "user_mngt" {
   type        = "zip"
-  source_dir  = "${path.root}/src/code/user_mngt"
-  output_path = "${path.root}/src/zip/user_mngt.zip"
+  source_dir  = "${path.root}/backend/code/user_mngt"
+  output_path = "${path.root}/backend/zip/user_mngt.zip"
 }
